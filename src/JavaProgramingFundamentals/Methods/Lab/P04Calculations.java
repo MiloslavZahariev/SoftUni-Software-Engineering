30cf8d93a7d66e557a34363bebe072f0029d8f42
Miloslav <horrorful@gmail.com>
2022-06-08 20:47:44 +0300

    First Commit

---
+++ src/JavaProgramingFundamentals/Methods/Lab/P04Calculations.java
@@ -0,0 +1,47 @@
+package JavaProgramingFundamentals.Methods.Lab;
+
+import java.util.Scanner;
+
+public class P04Calculations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String actionInput = scanner.nextLine();
+
+        int firstNumInput = Integer.parseInt(scanner.nextLine());
+        int secondNumInput = Integer.parseInt(scanner.nextLine());
+
+        switch (actionInput) {
+            case "add":
+                printAddNumbers(firstNumInput,secondNumInput);
+                break;
+            case "multiply":
+                printMultiplyNumbers(firstNumInput,secondNumInput);
+                break;
+            case "subtract":
+                printSubtractNumbers(firstNumInput,secondNumInput);
+                break;
+            case "divide":
+                printDivideNumbers(firstNumInput,secondNumInput);
+                break;
+        }
+    }
+
+    public static void printAddNumbers(int firstNum, int secondNum){
+        int result = firstNum + secondNum;
+        System.out.println(result);
+    }
+    public static void printMultiplyNumbers(int firstNum, int secondNum){
+        int result = firstNum * secondNum;
+        System.out.println(result);
+    }
+    public static void printSubtractNumbers(int firstNum, int secondNum){
+        int result = firstNum - secondNum;
+        System.out.println(result);
+    }
+    public static void printDivideNumbers(int firstNum, int secondNum){
+        int result = firstNum / secondNum;
+        System.out.println(result);
+    }
+
+}
