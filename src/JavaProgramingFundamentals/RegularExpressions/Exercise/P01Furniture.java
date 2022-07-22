61614a8bba917f7ee396fe1175675e80b0d440b0
Miloslav <horrorful@gmail.com>
2022-07-22 19:04:42 +0300

    first commit

---
+++ src/JavaProgramingFundamentals/RegularExpressions/Exercise/P01Furniture.java
@@ -0,0 +1,46 @@
+package JavaProgramingFundamentals.RegularExpressions.Exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class P01Furniture {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        List<String> furnitureArr = new ArrayList<>();
+        double totalSum = 0;
+
+        String regex = ">>(?<furnitureName>\\w+)<<(?<price>\\d+.?\\d*)!(?<quantity>\\d+)";
+        Pattern pattern = Pattern.compile(regex);
+
+
+        String input = scanner.nextLine();
+        while (!input.equals("Purchase")) {
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.find()) {
+                String furniture = matcher.group("furnitureName");
+                double price = Double.parseDouble(matcher.group("price"));
+                int quantity = Integer.parseInt(matcher.group("quantity"));
+
+                furnitureArr.add(furniture);
+                totalSum += price * quantity;
+            }
+
+
+            input = scanner.nextLine();
+        }
+
+
+        System.out.println("Bought furniture:");
+
+        for (String name : furnitureArr) {
+            System.out.println(name);
+        }
+
+        System.out.printf("Total money spend: %.2f",totalSum);
+    }
+}
