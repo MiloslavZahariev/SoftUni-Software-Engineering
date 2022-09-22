30a0fc208f5cde8ebaa09ed027c3aaf3ada2f46a
Miloslav <horrorful@gmail.com>
2022-09-22 12:32:32 +0300

    first commit

--- src/JavaProgramingAdvanced/StackAndQueues/Exercise/P09PoisonousPlants.java
+++ src/JavaProgramingAdvanced/StackAndQueues/Exercise/P09PoisonousPlants.java
@@ -1,9 +1,55 @@
 package JavaProgramingAdvanced.StackAndQueues.Exercise;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class P09PoisonousPlants {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int numberOfFlowers = Integer.parseInt(scanner.nextLine());
+
+        int[] flowers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        Deque<Integer> tempStack = new ArrayDeque<>();
+
+        int dayCounter =0;
+
+        for (int i = 0; i < numberOfFlowers; i++) {
+            stack.push(flowers[i]);
+        }
+        while (true) {
+            int currentStackSize = stack.size();
+            for (int i = 0; i < currentStackSize; i++) {
+                int currentFlower = stack.pop();
+                if (!stack.isEmpty() && currentFlower > stack.peek()) {
+
+                } else {
+                    tempStack.push(currentFlower);
+                }
+            }
+            if (!tempStack.isEmpty()) {
+                int currentTempStackSize = tempStack.size();{
+                    if(currentStackSize == currentTempStackSize){
+                        break;
+                    }
+                }
+                for (int i = 0; i < currentTempStackSize; i++) {
+                    if (!tempStack.isEmpty()) {
+                        stack.push(tempStack.pop());
+                    }
+                }
+            } else {
+                break;
+            }
+
+            dayCounter++;
+        }
+
+        System.out.println(dayCounter);
     }
 }
--- src/JavaProgramingAdvanced/StackAndQueues/Exercise/P10Robotics.java
+++ src/JavaProgramingAdvanced/StackAndQueues/Exercise/P10Robotics.java
@@ -1,9 +1,72 @@
 package JavaProgramingAdvanced.StackAndQueues.Exercise;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class P10Robotics {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String[] robots = scanner.nextLine().split(";");
+        String[] startingTime = scanner.nextLine().split(":");
+        int[] workingTime = new int[robots.length];
+
+        int startingTimeInSeconds = convertTimeToSeconds(startingTime);
+
+        Deque<String> detailsQueue = new ArrayDeque<>();
+        Map<String, Integer> busyRobotsMap = new LinkedHashMap<>();
+
+
+        String details = scanner.nextLine();
+
+        while (!details.equals("End")) {
+            detailsQueue.offer(details);
+            details = scanner.nextLine();
+        }
+
+        while (!detailsQueue.isEmpty()) {
+            startingTimeInSeconds++;
+
+            String detail = detailsQueue.poll();
+            boolean isWorking = false;
+            for (int i = 0; i < robots.length; i++) {
+
+                if (workingTime[i] == 0 && !isWorking) {
+                    workingTime[i] = Integer.parseInt(robots[i].split("-")[1]);
+                    isWorking = true;
+                    System.out.printf("%s - %s %s%n", robots[i].split("-")[0], detail, convertSecondToTime(startingTimeInSeconds));
+                }
+                if (workingTime[i] > 0) {
+                    workingTime[i]--;
+                }
+            }
+
+            if (!isWorking) {
+                detailsQueue.offer(detail);
+            }
+
+
+        }
+
+    }
+
+    private static String convertSecondToTime(int startingTimeInSeconds) {
+
+        int hours = startingTimeInSeconds / 3600 % 24;
+        startingTimeInSeconds %= 3600;
+
+        int minutes = startingTimeInSeconds / 60;
+        startingTimeInSeconds %= 60;
+
+        return String.format("[%02d:%02d:%02d]", hours, minutes, startingTimeInSeconds);
+
+    }
+
+    private static int convertTimeToSeconds(String[] startingTime) {
+        int seconds = Integer.parseInt(startingTime[2]);
+        int minutes = Integer.parseInt(startingTime[1]) * 60;
+        int hours = Integer.parseInt(startingTime[0]) * 3600;
+
+        return seconds + minutes + hours;
+
     }
 }
