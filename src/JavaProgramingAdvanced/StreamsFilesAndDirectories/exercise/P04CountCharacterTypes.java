10671ee26fc09a67ce14999ab02d994dee2f3204
Miloslav <horrorful@gmail.com>
2022-09-26 20:23:29 +0300

    first commit

---
+++ src/JavaProgramingAdvanced/StreamsFilesAndDirectories/exercise/P04CountCharacterTypes.java
@@ -0,0 +1,45 @@
+package JavaProgramingAdvanced.StreamsFilesAndDirectories.exercise;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class P04CountCharacterTypes {
+    public static void main(String[] args) throws IOException {
+
+        String path = "resources/01._Sum Lines_Ресурси/04. Java-Advanced-Files-and-Streams-Exercises-Resources/input.txt";
+
+        FileReader fileReader = new FileReader(path);
+
+        int vowels = 0;
+        int consonants = 0;
+        int punctuation = 0;
+
+        int oneByte = fileReader.read();
+
+        while (oneByte >= 0) {
+            char symbol = (char) oneByte;
+            if (isVowel(symbol)) {
+                vowels++;
+            } else if (isPunctuation(symbol)) {
+                punctuation++;
+            } else if (!Character.isWhitespace(symbol)) {
+                consonants++;
+            }
+            oneByte = fileReader.read();
+        }
+        fileReader.close();
+        System.out.printf("Vowels: %d%n", vowels);
+        System.out.printf("Consonants: %d%n", consonants);
+        System.out.printf("Punctuation: %d%n", punctuation);
+    }
+
+
+    private static boolean isPunctuation(char symbol) {
+        return symbol == '!' || symbol == '.' || symbol == ',' || symbol == '?';
+    }
+
+    private static boolean isVowel(char symbol) {
+        return (symbol == 'a' || symbol == 'e' || symbol == 'i' || symbol == 'o' || symbol == 'u');
+    }
+}
