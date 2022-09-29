2aaf8b195b15edd433558369f64c34131ee4c8a5
Miloslav <horrorful@gmail.com>
2022-09-29 20:30:12 +0300

    first commit

---
+++ src/JavaProgramingAdvanced/FunctionalProgramming/Exercise/P05ReverseAndExclude.java
@@ -0,0 +1,25 @@
+package JavaProgramingAdvanced.FunctionalProgramming.Exercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P05ReverseAndExclude {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> list = Arrays.stream(scanner.nextLine().split(" +"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int magicNumber = Integer.parseInt(scanner.nextLine());
+
+        Collections.reverse(list);
+
+        list.removeIf(n -> n % magicNumber == 0);
+
+        list.forEach(e -> System.out.print(e + " "));
+    }
+}
