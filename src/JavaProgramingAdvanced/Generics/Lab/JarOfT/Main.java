c1e40958fb2f2ccf651a8ea796ca13a518b280e3
Miloslav <horrorful@gmail.com>
2022-10-08 19:18:20 +0300

    first commit

---
+++ src/JavaProgramingAdvanced/Generics/Lab/JarOfT/Main.java
@@ -0,0 +1,11 @@
+package JavaProgramingAdvanced.Generics.Lab.JarOfT;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Jar<Integer> stringJar = new Jar<>();
+
+        stringJar.add(13);
+    }
+
+}
