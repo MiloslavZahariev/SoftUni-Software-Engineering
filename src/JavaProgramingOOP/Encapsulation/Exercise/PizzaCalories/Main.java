e7db20263ea6541516fd8d8379471baf57893f92
Miloslav <horrorful@gmail.com>
2022-11-04 17:11:32 +0200

    first commit

---
+++ src/JavaProgramingOOP/Encapsulation/Exercise/PizzaCalories/Main.java
@@ -0,0 +1,41 @@
+package JavaProgramingOOP.Encapsulation.Exercise.PizzaCalories;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] pizzaInfo = scanner.nextLine().split(" ");
+        String pizzaName = pizzaInfo[1];
+        int numberOfToppings = Integer.parseInt(pizzaInfo[2]);
+
+        String[] doughInfo = scanner.nextLine().split(" ");
+        String flourType = doughInfo[1];
+        String bakingTechnique = doughInfo[2];
+        double weight = Double.parseDouble(doughInfo[3]);
+
+
+        try {
+            Pizza pizza = new Pizza(pizzaName, numberOfToppings);
+            Dough dough = new Dough(flourType, bakingTechnique, weight);
+            pizza.setDough(dough);
+
+            String command = scanner.nextLine();
+            while (!command.equals("END")) {
+                String[] toppingInfo = command.split(" ");
+                String name = toppingInfo[1];
+                double toppingWeight = Double.parseDouble(toppingInfo[2]);
+                Topping topping = new Topping(name, toppingWeight);
+                pizza.addTopping(topping);
+                command = scanner.nextLine();
+            }
+            System.out.printf("%s - %.2f",pizza.getName(),pizza.getOverallCalories());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+    }
+}
